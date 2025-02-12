d1dc9ab6f244e5b9ee819ea3eb31889c2830ed59
Lily <alcarrillomunoz@gmail.com>
2025-02-12 14:46:15 -0500

    Authentication with Passport

--- models/User.js
+++ models/User.js
@@ -1,34 +1,34 @@
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 // const jwt = require("jsonwebtoken");
 
-// const UserSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: [true, "Please provide a name"],
-//     minlength: 3,
-//     maxlength: 50,
-//   },
-//   email: {
-//     type: String,
-//     required: [true, "Please provide an email"],
-//     match: [
-//       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-//       "Please provide a valid email",
-//     ],
-//     unique: true,
-//   },
-//   password: {
-//     type: String,
-//     required: [true, "Please provide a password"],
-//     minlength: 6,
-//   },
-// });
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 6,
+  },
+});
 
-// UserSchema.pre("save", async function () {
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-// });
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 // UserSchema.methods.createJWT = function () {
 //   return jwt.sign(
@@ -38,9 +38,9 @@
 //   );
 // };
 
-// UserSchema.methods.comparePassword = async function (candidatePassword) {
-//   const isMatch = await bcrypt.compare(candidatePassword, this.password);
-//   return isMatch;
-// };
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
 
-// module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
